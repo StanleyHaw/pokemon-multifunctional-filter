18e13d2bdef3ecd1b6d63c3674e1a62590876bda
StanleyHaw <stanleyhaw94@gmail.com>
2023-12-20 11:22:44 +0800

    feat: added filter-menu desktop layout

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import './main.scss';
 import './components/filter-display/filter-display.scss';
+import './components/filter-menu/filter-menu.scss';
 import './components/filter-results/filter-results.scss';
