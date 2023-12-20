81f41600edeaa0c4151403808466481eccaa3838
StanleyHaw <stanleyhaw94@gmail.com>
2023-12-20 14:18:17 +0800

    feat: added functional of toggle filter menu

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './main.scss';
 import './components/filter-display/filter-display.scss';
+import './components/filter-display/index.js';
 import './components/filter-menu/filter-menu.scss';
 import './components/filter-results/filter-results.scss';
