3f089fb34502c6987d35461a7e150cb6b68614e3
StanleyHaw <stanleyhaw94@gmail.com>
2023-12-22 11:42:47 +0800

    feat: added filter-results RWD layout for mobile-sized devices

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './main.scss';
 import './components/filter-display/filter-display.scss';
 import './components/filter-display/index.js';
+import './components/filter-results/index.js';
 import './components/filter-menu/filter-menu.scss';
 import './components/filter-results/filter-results.scss';
