bf0c218df5905b75a0488e7ebb5ee7e9c82b877c
StanleyHaw <stanleyhaw94@gmail.com>
2023-12-25 23:08:49 +0800

    fix: optimized scroll style for species-filter and pokemon-filter

--- src/components/filter-display/index.js
+++ src/components/filter-display/index.js
@@ -1,54 +1,56 @@
-const filterMenuToggleButton = document.querySelector('.filter-button');
-const filterDisplay = document.querySelector('.filter-display');
-const filterArrowIcon = filterMenuToggleButton.querySelector('.fa-angle-left');
-const filterMenu = document.querySelector('.filter-menu');
-const filterResultHead = document.querySelector('.filter-result-head');
-const filterResultBody = document.querySelector('.filter-result-body');
-const filterMenuCloseButton = document.querySelector('.close-menu');
+import './scroll-inset-shadow.js'
+
+const filterMenuToggleButton = document.querySelector('.filter-button')
+const filterDisplay = document.querySelector('.filter-display')
+const filterArrowIcon = filterMenuToggleButton.querySelector('.fa-angle-left')
+const filterMenu = document.querySelector('.filter-menu')
+const filterResultHead = document.querySelector('.filter-result-head')
+const filterResultBody = document.querySelector('.filter-result-body')
+const filterMenuCloseButton = document.querySelector('.close-menu')
 
 function toggleFilterMenu() {
-  const isExpanded = filterDisplay.classList.contains('display-expand');
+  const isExpanded = filterDisplay.classList.contains('display-expand')
 
-  filterDisplay.classList.toggle('display-expand', !isExpanded);
-  filterDisplay.classList.toggle('display-collapse', isExpanded);
-  filterDisplay.classList.add('display-transition');
+  filterDisplay.classList.toggle('display-expand', !isExpanded)
+  filterDisplay.classList.toggle('display-collapse', isExpanded)
+  filterDisplay.classList.add('display-transition')
 
-  filterArrowIcon.classList.toggle('fa-angle-left', !isExpanded);
-  filterArrowIcon.classList.toggle('fa-angle-right', isExpanded);
+  filterArrowIcon.classList.toggle('fa-angle-left', !isExpanded)
+  filterArrowIcon.classList.toggle('fa-angle-right', isExpanded)
 
-  filterMenu.classList.toggle('menu-visible', isExpanded);
-  filterMenu.classList.toggle('menu-invisible', !isExpanded);
+  filterMenu.classList.toggle('menu-visible', isExpanded)
+  filterMenu.classList.toggle('menu-invisible', !isExpanded)
 
-  filterResultHead.classList.toggle('result-expand', !isExpanded);
-  filterResultHead.classList.toggle('result-collapse', isExpanded);
-  filterResultHead.classList.add('result-transition');
+  filterResultHead.classList.toggle('result-expand', !isExpanded)
+  filterResultHead.classList.toggle('result-collapse', isExpanded)
+  filterResultHead.classList.add('result-transition')
 
-  filterResultBody.classList.toggle('result-expand', !isExpanded);
-  filterResultBody.classList.toggle('result-collapse', isExpanded);
-  filterResultBody.classList.add('result-transition');
+  filterResultBody.classList.toggle('result-expand', !isExpanded)
+  filterResultBody.classList.toggle('result-collapse', isExpanded)
+  filterResultBody.classList.add('result-transition')
 }
 
 function closeFilterMenu() {
-  filterDisplay.classList.remove('display-collapse');
-  filterDisplay.classList.add('display-expand');
-  filterDisplay.classList.add('display-transition');
+  filterDisplay.classList.remove('display-collapse')
+  filterDisplay.classList.add('display-expand')
+  filterDisplay.classList.add('display-transition')
 
-  filterArrowIcon.classList.remove('fa-angle-right');
-  filterArrowIcon.classList.add('fa-angle-left');
+  filterArrowIcon.classList.remove('fa-angle-right')
+  filterArrowIcon.classList.add('fa-angle-left')
 
-  filterMenu.classList.remove('menu-visible');
-  filterMenu.classList.add('menu-invisible');
+  filterMenu.classList.remove('menu-visible')
+  filterMenu.classList.add('menu-invisible')
 
-  filterResultHead.classList.remove('result-collapse');
-  filterResultHead.classList.add('result-expand');
-  filterResultHead.classList.add('result-transition');
+  filterResultHead.classList.remove('result-collapse')
+  filterResultHead.classList.add('result-expand')
+  filterResultHead.classList.add('result-transition')
 
-  filterResultBody.classList.remove('result-collapse');
-  filterResultBody.classList.add('result-expand');
-  filterResultBody.classList.add('result-transition');
+  filterResultBody.classList.remove('result-collapse')
+  filterResultBody.classList.add('result-expand')
+  filterResultBody.classList.add('result-transition')
 }
 
-filterMenuToggleButton.addEventListener('click', toggleFilterMenu);
-filterMenuCloseButton.addEventListener('click', closeFilterMenu);
+filterMenuToggleButton.addEventListener('click', toggleFilterMenu)
+filterMenuCloseButton.addEventListener('click', closeFilterMenu)
 // TODO: At resolutions of 1024px or below, pressing the `filterMenuToggleButton` will display an overlay.
 // Pressing the `filterMenuCloseButton` will make the overlay disappear.
