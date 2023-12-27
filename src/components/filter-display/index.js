250338678073ad2dc56b3675df717edaf9d7e66e
StanleyHaw <stanleyhaw94@gmail.com>
2023-12-27 09:20:20 +0800

    fix: fixed the impact on the screen during the transition

--- src/components/filter-display/index.js
+++ src/components/filter-display/index.js
@@ -1,56 +1,57 @@
-import './scroll-inset-shadow.js'
+import './scroll-inset-shadow.js';
 
-const filterMenuToggleButton = document.querySelector('.filter-button')
-const filterDisplay = document.querySelector('.filter-display')
-const filterArrowIcon = filterMenuToggleButton.querySelector('.fa-angle-left')
-const filterMenu = document.querySelector('.filter-menu')
-const filterResultHead = document.querySelector('.filter-result-head')
-const filterResultBody = document.querySelector('.filter-result-body')
-const filterMenuCloseButton = document.querySelector('.close-menu')
+const filterMenuToggleButton = document.querySelector('.filter-button');
+const filterDisplay = document.querySelector('.filter-display');
+const filterArrowIcon = filterMenuToggleButton.querySelector('.fa-angle-left');
+const filterMenu = document.querySelector('.filter-menu');
+const filterResultHead = document.querySelector('.filter-result-head');
+const filterResultBody = document.querySelector('.filter-result-body');
+const filterMenuCloseButton = document.querySelector('.close-menu');
+
+function toggleTransition(item) {
+  item.classList.add('run-transition');
+  setTimeout(function () {
+    item.classList.remove('run-transition');
+  }, 300);
+}
+
+function toggleCollapse(item, transitionClass, menuVisible) {
+  item.classList.toggle(`${transitionClass}-default-size`, !menuVisible);
+  item.classList.toggle(`${transitionClass}-collapse`, menuVisible);
+  toggleTransition(item);
+}
+
+function toggleVisibleAndInvisible(item, transitionClass, menuVisible) {
+  item.classList.toggle(`${transitionClass}-invisible`, !menuVisible);
+  item.classList.toggle(`${transitionClass}-visible`, menuVisible);
+  toggleTransition(item);
+}
 
 function toggleFilterMenu() {
-  const isExpanded = filterDisplay.classList.contains('display-expand')
+  const menuVisible = filterMenu.classList.contains('menu-invisible');
 
-  filterDisplay.classList.toggle('display-expand', !isExpanded)
-  filterDisplay.classList.toggle('display-collapse', isExpanded)
-  filterDisplay.classList.add('display-transition')
-
-  filterArrowIcon.classList.toggle('fa-angle-left', !isExpanded)
-  filterArrowIcon.classList.toggle('fa-angle-right', isExpanded)
-
-  filterMenu.classList.toggle('menu-visible', isExpanded)
-  filterMenu.classList.toggle('menu-invisible', !isExpanded)
-
-  filterResultHead.classList.toggle('result-expand', !isExpanded)
-  filterResultHead.classList.toggle('result-collapse', isExpanded)
-  filterResultHead.classList.add('result-transition')
-
-  filterResultBody.classList.toggle('result-expand', !isExpanded)
-  filterResultBody.classList.toggle('result-collapse', isExpanded)
-  filterResultBody.classList.add('result-transition')
+  toggleCollapse(filterDisplay, 'display', menuVisible);
+  toggleCollapse(filterResultHead, 'result', menuVisible);
+  toggleCollapse(filterResultBody, 'result', menuVisible);
+  toggleVisibleAndInvisible(filterMenu, 'menu', menuVisible);
+  filterArrowIcon.classList.toggle('fa-angle-left', !menuVisible);
+  filterArrowIcon.classList.toggle('fa-angle-right', menuVisible);
 }
 
 function closeFilterMenu() {
-  filterDisplay.classList.remove('display-collapse')
-  filterDisplay.classList.add('display-expand')
-  filterDisplay.classList.add('display-transition')
+  toggleCollapse(filterResultHead, 'result', false);
+  toggleCollapse(filterResultBody, 'result', false);
+  toggleVisibleAndInvisible(filterMenu, 'menu', false);
+  filterDisplay.classList.remove('display-collapse');
+  filterDisplay.classList.add('display-default-size');
 
-  filterArrowIcon.classList.remove('fa-angle-right')
-  filterArrowIcon.classList.add('fa-angle-left')
-
-  filterMenu.classList.remove('menu-visible')
-  filterMenu.classList.add('menu-invisible')
-
-  filterResultHead.classList.remove('result-collapse')
-  filterResultHead.classList.add('result-expand')
-  filterResultHead.classList.add('result-transition')
-
-  filterResultBody.classList.remove('result-collapse')
-  filterResultBody.classList.add('result-expand')
-  filterResultBody.classList.add('result-transition')
+  if (filterArrowIcon.classList.contains('fa-angle-right')) {
+    filterArrowIcon.classList.remove('fa-angle-right');
+    filterArrowIcon.classList.add('fa-angle-left');
+  }
 }
 
-filterMenuToggleButton.addEventListener('click', toggleFilterMenu)
-filterMenuCloseButton.addEventListener('click', closeFilterMenu)
+filterMenuToggleButton.addEventListener('click', toggleFilterMenu);
+filterMenuCloseButton.addEventListener('click', closeFilterMenu);
 // TODO: At resolutions of 1024px or below, pressing the `filterMenuToggleButton` will display an overlay.
 // Pressing the `filterMenuCloseButton` will make the overlay disappear.
