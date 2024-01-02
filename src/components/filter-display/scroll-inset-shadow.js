2ded16a472a85e5fa2e78db7f87f8c457a746983
StanleyHaw <stanleyhaw94@gmail.com>
2024-01-02 16:43:07 +0800

    refactor: refactored scroll-inset-shadow.js's logic

--- src/components/filter-display/scroll-inset-shadow.js
+++ src/components/filter-display/scroll-inset-shadow.js
@@ -1,39 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-  function setupScrollShadowEffect(
-    scrollWrapperClass,
-    shadowLeftClass,
-    shadowRightClass,
-  ) {
-    const scrollWrapper = document.querySelector(`.${scrollWrapperClass}`);
-    const shadowLeft = document.querySelector(`.${shadowLeftClass}`);
-    const shadowRight = document.querySelector(`.${shadowRightClass}`);
+const pokemonFilter = document.querySelector('.pokemon-filter');
+const pokemonScrollWrapper = pokemonFilter.querySelector('.scroll-wrapper');
+const pokemonLeftshadow = pokemonFilter.querySelector('.scroll-inset-shadow-left');
+const pokemonRightshadow = pokemonFilter.querySelector('.scroll-inset-shadow-right');
 
-    scrollWrapper.addEventListener('scroll', function () {
-      const isHidden = scrollWrapper.classList.contains('shadow-invisible');
-      if (scrollWrapper.scrollLeft === 0) {
-        shadowLeft.classList.toggle('shadow-invisible', !isHidden);
-        shadowRight.classList.toggle('shadow-invisible', isHidden);
-      } else if (
-        scrollWrapper.scrollLeft + scrollWrapper.clientWidth ===
-        scrollWrapper.scrollWidth
-      ) {
-        shadowLeft.classList.toggle('shadow-invisible', isHidden);
-        shadowRight.classList.toggle('shadow-invisible', !isHidden);
-      } else {
-        shadowLeft.classList.remove('shadow-invisible');
-        shadowRight.classList.remove('shadow-invisible');
-      }
-    });
+const speciesFilter = document.querySelector('.species-filter');
+const speciesScrollWrapper = speciesFilter.querySelector('.scroll-wrapper');
+const speciesLeftshadow = speciesFilter.querySelector('.scroll-inset-shadow-left');
+const speciesRightshadow = speciesFilter.querySelector('.scroll-inset-shadow-right');
+
+function setupScrollShadowEffect(scrollWrapper, shadowLeft, shadowRight) {
+  const isHidden = scrollWrapper.classList.contains('shadow-invisible');
+  if (scrollWrapper.scrollLeft === 0) {
+    shadowLeft.classList.toggle('shadow-invisible', !isHidden);
+    shadowRight.classList.toggle('shadow-invisible', isHidden);
+  } else if (scrollWrapper.scrollLeft + scrollWrapper.clientWidth === scrollWrapper.scrollWidth) {
+    shadowLeft.classList.toggle('shadow-invisible', isHidden);
+    shadowRight.classList.toggle('shadow-invisible', !isHidden);
+  } else {
+    shadowLeft.classList.remove('shadow-invisible');
+    shadowRight.classList.remove('shadow-invisible');
   }
+}
 
-  setupScrollShadowEffect(
-    'pokemon-filter .scroll-wrapper',
-    'pokemon-filter .scroll-inset-shadow-left',
-    'pokemon-filter .scroll-inset-shadow-right',
-  );
-  setupScrollShadowEffect(
-    'species-filter .scroll-wrapper',
-    'species-filter .scroll-inset-shadow-left',
-    'species-filter .scroll-inset-shadow-right',
-  );
+pokemonScrollWrapper.addEventListener('scroll', () => {
+  setupScrollShadowEffect(pokemonScrollWrapper, pokemonLeftshadow, pokemonRightshadow);
+});
+
+speciesScrollWrapper.addEventListener('scroll', () => {
+  setupScrollShadowEffect(speciesScrollWrapper, speciesLeftshadow, speciesRightshadow);
 });
