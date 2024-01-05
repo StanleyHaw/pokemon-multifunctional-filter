031bf71418d0eb929828f969621f3cf43fd19110
StanleyHaw <stanleyhaw94@gmail.com>
2024-01-05 15:47:23 +0800

    feat: fetched API and create UI

---
+++ src/components/filter-results/create-ability-element.js
@@ -0,0 +1,47 @@
+import { judgeNullValue } from './judge-forme-value.js';
+
+export function createSingleAbilityElement(ability) {
+  const pokemonAbility = document.createElement('div');
+  pokemonAbility.classList.add('result', 'result-ability-single');
+
+  const pokemonAbilityParagraph = document.createElement('p');
+  pokemonAbilityParagraph.textContent = ability;
+
+  pokemonAbility.appendChild(pokemonAbilityParagraph);
+
+  return pokemonAbility;
+}
+
+export function createDoubleAbilitiesElement(ability1, ability2) {
+  const pokemonAbility1 = document.createElement('div');
+  pokemonAbility1.classList.add('result', 'result-ability-double');
+
+  const pokemonAbilityParagraph1 = document.createElement('p');
+  pokemonAbilityParagraph1.textContent = ability1;
+
+  const pokemonAbility2 = document.createElement('div');
+  pokemonAbility2.classList.add('result', 'result-ability-double');
+
+  const pokemonAbilityParagraph2 = document.createElement('p');
+  pokemonAbilityParagraph2.textContent = ability2;
+
+  pokemonAbility1.appendChild(pokemonAbilityParagraph1);
+  pokemonAbility2.appendChild(pokemonAbilityParagraph2);
+
+  const pokemonAbilities = [pokemonAbility1, pokemonAbility2];
+
+  return pokemonAbilities;
+}
+
+export function createHiddenAbilityElement(hiddenAbility) {
+  const pokemonHiddenAbility = document.createElement('div');
+  pokemonHiddenAbility.classList.add('result', 'result-ability-hidden');
+
+  const pokemonHiddenAbilityParagraph = document.createElement('p');
+  pokemonHiddenAbilityParagraph.textContent = hiddenAbility;
+  judgeNullValue(hiddenAbility, pokemonHiddenAbility);
+
+  pokemonHiddenAbility.appendChild(pokemonHiddenAbilityParagraph);
+
+  return pokemonHiddenAbility;
+}
---
+++ src/components/filter-results/type-element-creator.js
@@ -0,0 +1,44 @@
+export function createSingleTypeElement(type) {
+  const pokemonType = document.createElement('div');
+  pokemonType.classList.add('result', `type-${type}`, 'result-type-single');
+
+  const pokemonTypeIcon = document.createElement('img');
+  pokemonTypeIcon.src = `./src/assets/img/type/pokemon-type-${type}.png`;
+
+  const pokemonTypeParagraph = document.createElement('p');
+  pokemonTypeParagraph.textContent = type;
+
+  pokemonType.appendChild(pokemonTypeIcon);
+  pokemonType.appendChild(pokemonTypeParagraph);
+
+  return pokemonType;
+}
+
+export function createDoubleTypesElement(type1, type2) {
+  const pokemonType1 = document.createElement('div');
+  pokemonType1.classList.add('result', `type-${type1}`, 'result-type-double');
+
+  const pokemonTypeIcon1 = document.createElement('img');
+  pokemonTypeIcon1.src = `./src/assets/img/type/pokemon-type-${type1}.png`;
+
+  const pokemonTypeParagraph1 = document.createElement('p');
+  pokemonTypeParagraph1.textContent = type1;
+
+  const pokemonType2 = document.createElement('div');
+  pokemonType2.classList.add('result', `type-${type2}`, 'result-type-double');
+
+  const pokemonTypeIcon2 = document.createElement('img');
+  pokemonTypeIcon2.src = `./src/assets/img/type/pokemon-type-${type2}.png`;
+
+  const pokemonTypeParagraph2 = document.createElement('p');
+  pokemonTypeParagraph2.textContent = type2;
+
+  pokemonType1.appendChild(pokemonTypeIcon1);
+  pokemonType1.appendChild(pokemonTypeParagraph1);
+  pokemonType2.appendChild(pokemonTypeIcon2);
+  pokemonType2.appendChild(pokemonTypeParagraph2);
+
+  const pokemonTypes = [pokemonType1, pokemonType2];
+
+  return pokemonTypes;
+}
