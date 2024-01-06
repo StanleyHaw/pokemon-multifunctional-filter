aa168362fdb4525c4e686fe3633671e274ca1146
AlbertNotAlien <albert830207@gmail.com>
2024-01-07 00:05:15 +0800

    chore: fixed ineffective ESLint configuration

--- src/components/filter-display/scroll-inset-shadow.js
+++ src/components/filter-display/scroll-inset-shadow.js
@@ -1,19 +1,30 @@
 const pokemonFilter = document.querySelector('.pokemon-filter');
 const pokemonScrollWrapper = pokemonFilter.querySelector('.scroll-wrapper');
-const pokemonLeftshadow = pokemonFilter.querySelector('.scroll-inset-shadow-left');
-const pokemonRightshadow = pokemonFilter.querySelector('.scroll-inset-shadow-right');
+const pokemonLeftshadow = pokemonFilter.querySelector(
+  '.scroll-inset-shadow-left'
+);
+const pokemonRightshadow = pokemonFilter.querySelector(
+  '.scroll-inset-shadow-right'
+);
 
 const speciesFilter = document.querySelector('.species-filter');
 const speciesScrollWrapper = speciesFilter.querySelector('.scroll-wrapper');
-const speciesLeftshadow = speciesFilter.querySelector('.scroll-inset-shadow-left');
-const speciesRightshadow = speciesFilter.querySelector('.scroll-inset-shadow-right');
+const speciesLeftshadow = speciesFilter.querySelector(
+  '.scroll-inset-shadow-left'
+);
+const speciesRightshadow = speciesFilter.querySelector(
+  '.scroll-inset-shadow-right'
+);
 
 function setupScrollShadowEffect(scrollWrapper, shadowLeft, shadowRight) {
   const isHidden = scrollWrapper.classList.contains('shadow-invisible');
   if (scrollWrapper.scrollLeft === 0) {
     shadowLeft.classList.toggle('shadow-invisible', !isHidden);
     shadowRight.classList.toggle('shadow-invisible', isHidden);
-  } else if (scrollWrapper.scrollLeft + scrollWrapper.clientWidth === scrollWrapper.scrollWidth) {
+  } else if (
+    scrollWrapper.scrollLeft + scrollWrapper.clientWidth ===
+    scrollWrapper.scrollWidth
+  ) {
     shadowLeft.classList.toggle('shadow-invisible', isHidden);
     shadowRight.classList.toggle('shadow-invisible', !isHidden);
   } else {
@@ -23,9 +34,17 @@ function setupScrollShadowEffect(scrollWrapper, shadowLeft, shadowRight) {
 }
 
 pokemonScrollWrapper.addEventListener('scroll', () => {
-  setupScrollShadowEffect(pokemonScrollWrapper, pokemonLeftshadow, pokemonRightshadow);
+  setupScrollShadowEffect(
+    pokemonScrollWrapper,
+    pokemonLeftshadow,
+    pokemonRightshadow
+  );
 });
 
 speciesScrollWrapper.addEventListener('scroll', () => {
-  setupScrollShadowEffect(speciesScrollWrapper, speciesLeftshadow, speciesRightshadow);
+  setupScrollShadowEffect(
+    speciesScrollWrapper,
+    speciesLeftshadow,
+    speciesRightshadow
+  );
 });
