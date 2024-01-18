dc3261961407d714b6f24423c77a3cb9824cf1eb
AlbertNotAlien <albert830207@gmail.com>
2024-01-19 03:24:22 +0800

    wip2

--- src/components/filter-results/create-element/create-pokemon-list.js
+++ src/components/filter-results/create-element/create-pokemon-list.js
@@ -1,10 +1,5 @@
 import { convertToCleanString } from '../../../utils/convert-string.js';
-import {
-  serialNumberElement,
-  pokemonImageElement,
-  pokemonNameElement,
-  pokemonFormElement,
-} from './idenfity-result/create-identify-result.js';
+import { appendInfoBlock } from './idenfity-result/create-identify-result.js';
 import {
   renderSpeciesTypeElement,
   speciesAbilityElement,
@@ -17,13 +12,17 @@ export function createPokemonIdentifyElement(data) {
   const resultIdentifyWrapper = document.createElement('div');
   const pokemonName = convertToCleanString(data.name);
 
-  resultIdentifyContainer.appendChild(resultIdentifyWrapper);
-  resultIdentifyWrapper.appendChild(serialNumberElement(data));
-  resultIdentifyWrapper.appendChild(pokemonImageElement(data));
-  resultIdentifyWrapper.appendChild(pokemonNameElement(data));
-  resultIdentifyWrapper.appendChild(pokemonFormElement(data));
+  const { name, num, baseForme, forme } = data;
+  const serialNumber = num.toString().padStart(4, '0');
+  const imgName = data.name.toLowerCase().replace(/[^a-z0-9]/g, '');
+  const formeContent = baseForme || forme || '';
 
   resultIdentifyWrapper.classList.add('result-identify', `${pokemonName}`);
+  resultIdentifyContainer.appendChild(resultIdentifyWrapper);
+  appendInfoBlock(resultIdentifyWrapper, 'serial-number', serialNumber);
+  appendInfoBlock(resultIdentifyWrapper, 'pokemon-image', imgName);
+  appendInfoBlock(resultIdentifyWrapper, 'pokemon-name', name);
+  appendInfoBlock(resultIdentifyWrapper, 'regional-form', formeContent);
 
   return resultIdentifyWrapper;
 }
--- src/utils/components/judge-forme-result.js
+++
@@ -1,3 +0,0 @@
-export function judgeFormeContent(data, pokemonFormResult) {
-  pokemonFormResult.textContent = data.baseForme ? data.baseForme : data.forme ? data.forme : '';
-}
